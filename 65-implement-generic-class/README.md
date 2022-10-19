9c14b6de7c3abe480dd6ddd5e976c615871eb9df
pianinator <113355583+pianinator@users.noreply.github.com>
2022-10-19 20:53:17 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@5e45cad87f13b8cb8aacdef6c0ee9821f7b78e92 🚀

---
+++ 65-implement-generic-class/README.md
@@ -0,0 +1,11 @@
+# INSTRUCTIONS
+
+1. Implement interface Graph
+2. Complete the test UseGraph
+3. Run the test and understand the outcome
+
+**Bonus**: implement a Graph where the exploration strategy can be selected arbitrarily.
+Implement both a depth-first strategy and a breadth-first strategy.
+Helpful readings:
+* http://artint.info/html/ArtInt_51.html
+* https://en.wikipedia.org/wiki/Strategy_pattern
---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/UseGraph.java
@@ -0,0 +1,71 @@
+package it.unibo.generics.graph;
+
+import it.unibo.generics.graph.api.Graph;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ *
+ */
+public final class UseGraph {
+
+    private UseGraph() {
+    }
+
+    /**
+     * @param args
+     *            ignored
+     */
+    public static void main(final String... args) {
+        /*
+         * Test your graph implementation(s) by calling testGraph
+         */
+        testGraph(null);
+    }
+
+    private static void testGraph(final Graph<String> graph) {
+        graph.addNode("a");
+        graph.addNode("b");
+        graph.addNode("c");
+        graph.addNode("d");
+        graph.addNode("e");
+        graph.addEdge("a", "b");
+        graph.addEdge("b", "c");
+        graph.addEdge("c", "d");
+        graph.addEdge("d", "e");
+        graph.addEdge("c", "a");
+        graph.addEdge("e", "a");
+        /*
+         * Should be ["a","b","c","d","e"], in any order
+         */
+        assertIsAnyOf(graph.nodeSet(), Set.of(splitOnWhiteSpace("a b c d e")));
+        /*
+         * ["d","a"], in any order
+         */
+        assertIsAnyOf(graph.linkedNodes("c"), Set.of(splitOnWhiteSpace("a d")));
+        /*
+         * Either the path b,c,a or b,c,d,e,a
+         */
+        assertIsAnyOf(
+            graph.getPath("b", "a"),
+            Arrays.asList(splitOnWhiteSpace("b c a")),
+            Arrays.asList(splitOnWhiteSpace("b c d e a"))
+        );
+    }
+
+    private static void assertIsAnyOf(final Object actual, final Object... valid) {
+        for (final var target: Objects.requireNonNull(valid)) {
+            if (Objects.equals(target, actual)) {
+                System.out.println("OK: " + actual + " matches " + target); // NOPMD
+                return;
+            }
+        }
+        throw new AssertionError("None of " + Arrays.asList(valid) + " matches " + actual);
+    }
+
+    private static String[] splitOnWhiteSpace(final String target) {
+        return target.split("\\s+");
+    }
+}
