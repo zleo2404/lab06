738b979cb4e5a9eff9fb74d08f11e5c08f9784df
pianinator <113355583+pianinator@users.noreply.github.com>
2023-11-14 17:52:36 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@0e0055050131b68df574a54f34f20aadedc0c42a 🚀

--- 63-exceptions/build.gradle.kts
+++ 63-exceptions/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.registering
 plugins {
     application
     java
-    id("org.danilopianini.gradle-java-qa") version "1.25.0"
+    id("org.danilopianini.gradle-java-qa") version "1.24.1"
 }
 
 repositories {
